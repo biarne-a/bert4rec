9032f086d3e290a44d398f6a8aa7d56ea42f0af2
Adrien Biarnes <biarnes.adrien@gmail.com>
2024-07-31 17:37:10 +0000

    make gru4rec work with TF2.16

--- train/run.py
+++ train/run.py
@@ -49,12 +49,12 @@ def run_training(config: Config):
     history = model.fit(
         x=data.train_ds,
         epochs=1_000,
-        steps_per_epoch=5_000,
+        steps_per_epoch=data.nb_train // config.batch_size,
         validation_data=data.val_ds,
         validation_steps=data.nb_val // config.batch_size,
         callbacks=[
             tf.keras.callbacks.TensorBoard(log_dir="logs", update_freq=100),
-            tf.keras.callbacks.EarlyStopping(monitor="val_fixed_loss", mode="min", patience=15),
+            tf.keras.callbacks.EarlyStopping(monitor="val_fixed_loss", mode="min", patience=1),
             tf.keras.callbacks.ModelCheckpoint(save_filepath, monitor="val_fixed_loss", mode="min", save_best_only=True),
         ],
         verbose=1,
