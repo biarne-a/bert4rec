remove pooler + potential fix huge error on loss computation (forgot to apply the sample weights)

bert4rec/bert4rec_model.py
@@ -41,8 +41,7 @@ class BERT4RecModel(tf.keras.Model):
             "input_ids": inputs["input_ids"],
             "input_mask": inputs["input_mask"],
         }
-        encoder_outputs = self.encoder(encoder_inputs, training=training)
-        sequence_output = encoder_outputs["sequence_output"]
+        sequence_output = self.encoder(encoder_inputs, training=training)
         masked_lm_positions = inputs["masked_lm_positions"]
         return self.masked_lm(sequence_output, masked_lm_positions)
 
@@ -52,7 +51,7 @@ class BERT4RecModel(tf.keras.Model):
         sample_weight = inputs["masked_lm_weights"]
         with tf.GradientTape() as tape:
             y_pred = self(inputs, training=True)
-            loss = self.compute_loss(inputs, y_true, y_pred, training=True)
+            loss = self.compute_loss(inputs, y_true, y_pred, sample_weight=sample_weight)
 
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
@@ -69,7 +68,7 @@ class BERT4RecModel(tf.keras.Model):
         sample_weight = inputs["masked_lm_weights"]
         y_pred = self(inputs, training=False)
 
-        loss = self.compute_loss(inputs, y_true, y_pred, training=False)
+        loss = self.compute_loss(inputs, y_true, y_pred, sample_weight=sample_weight)
         self.compiled_metrics.update_state(y_true, y_pred, sample_weight=sample_weight)
 
         metric_values = {m.name: m.result() for m in self.metrics}

bert4rec/layers/bert4rec_encoder.py
@@ -155,12 +155,6 @@ class Bert4RecEncoder(tf.keras.layers.Layer):
                 name='transformer_layer_%d' % i)
             self._transformer_layers.append(layer)
 
-        self._pooler_layer = tf.keras.layers.Dense(
-            units=hidden_size,
-            activation='tanh',
-            kernel_initializer=utils.clone_initializer(initializer),
-            name='pooler_transform')
-
         self._config = {
             'vocab_size': vocab_size,
             'hidden_size': hidden_size,
@@ -231,14 +225,7 @@ class Bert4RecEncoder(tf.keras.layers.Layer):
             encoder_outputs.append(x)
 
         last_encoder_output = encoder_outputs[-1]
-        first_token_tensor = last_encoder_output[:, 0, :]
-        pooled_output = self._pooler_layer(first_token_tensor)
-
-        return dict(
-            sequence_output=encoder_outputs[-1],
-            pooled_output=pooled_output,
-            encoder_outputs=encoder_outputs
-        )
+        return last_encoder_output
 
     def get_embedding_table(self):
         return self._embedding_layer.embeddings